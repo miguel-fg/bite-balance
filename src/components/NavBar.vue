65d795903ae05986d74bf55f385478f9edf9c0dc
miguel-fg <miguelfierro55@gmail.com>
2024-11-04 11:45:28 -0800

    Remove svg loader

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,11 @@
   <div class="flex px-16 md:px-20 lg:px-48 justify-between items-center py-1 bg-primary-800">
     <RouterLink to="/">
       <div class="flex items-center gap-1">
-        <img src="../../public/bb_logo.png" alt="Bite Balance logo" class="w-8 h-8 md:w-10 md:h-10">
-        <span class="font-MT font-bold text-body text-white">Bite Balance</span>
+        <img src="/bb_logo.png" alt="Bite Balance logo" class="w-8 h-8 md:w-10 md:h-10">
+        <span class="hidden lg:flex font-MT font-bold text-body text-white">Bite Balance</span>
       </div>
     </RouterLink>
-    <div class="flex">
+    <div class="hidden md:flex">
       <nav class="font-HM font-light underline text-white">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about" class="ml-10">About</RouterLink>
@@ -17,9 +17,26 @@
         <RouterLink to="/signup"><span class="px-4 py-2 rounded-md bg-transparent border-white border-2 font-HM font-semibold text-body text-white">Sign up</span></RouterLink>
       </div>
     </div>
+    <div class="flex md:hidden">
+      <button>
+        <MenuIcon class="w-6 h-5 overflow-visible"/>
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import MenuIcon from '../assets/icons/menu 1.svg'
+  import CloseIcon from '../assets/icons/close 1.svg'
+  import { ref } from 'vue'
 
+  const isOpen = ref(false);
+
+  const openNav = () => {
+    isOpen.value = true;
+  }
+
+  const closeNav = () => {
+    isOpen.value = false;
+  }
 </script>
