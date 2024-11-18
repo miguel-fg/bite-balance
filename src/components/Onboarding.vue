e9587d8f80eebd4b66c4384f804c3217ac365585
miguel-fg <miguelfierro55@gmail.com>
2024-11-17 16:57:18 -0800

    Style onboarding welcome screen
    Progress nodes will be styled separately

--- src/components/Onboarding.vue
+++ src/components/Onboarding.vue
@@ -1,17 +1,37 @@
 <template>
-  <div v-if="props.visible">
-    <div v-if="currentQuestion === 0">
-      <h1>Welcome to Bite Balance</h1>
-      <h2>Let's get to know you better!</h2>
-      <p>
-        Answer these quick questions to personalize your nutrition tracker
-        experience.
-      </p>
-      <div>
-        <button @click="$emit('closeOnboarding')">Skip</button>
-        <button @click="nextQuestion">Get Started</button>
+  <div
+    v-if="props.visible"
+    class="z-50 inset-0 fixed flex flex-col justify-center px-4 bg-ui-gray-100"
+  >
+    <!-- Welcome to Onboarding quiz -->
+    <div v-if="currentQuestion === 0" class="flex flex-col gap-12">
+      <h1 class="font-MT font-bold text-title text-ui-gray-900">
+        Welcome to Bite Balance
+      </h1>
+      <img src="/bb_logo_dark.png" alt="Bite Balance logo" class="w-24 h-24" />
+      <div class="flex flex-col gap-1">
+        <h1 class="font-HK font-regular text-subtitle text-ui-gray-900">
+          Let's get to know you better!
+        </h1>
+        <p class="font-HM font-light text-body text-ui-gray-700">
+          Answer these quick questions to personalize your nutrition tracker
+          experience.
+        </p>
+      </div>
+      <div class="flex justify-end gap-4">
+        <button @click="$emit('closeOnboarding')" class="btn text-ui-gray-700">
+          Skip
+        </button>
+        <button
+          @click="nextQuestion"
+          class="btn bg-primary-500 text-ui-gray-900 shadow"
+        >
+          Get Started
+        </button>
       </div>
     </div>
+
+    <!-- Summary of Onboarding user answers -->
     <div v-else-if="currentQuestion === 5">
       <h1>Summary</h1>
       <p><span>Date of Birth:</span> {{ dateOfBirth }}</p>
@@ -23,38 +43,44 @@
         <button @click="$emit('closeOnboarding')">Finish</button>
       </div>
     </div>
-    <OnboardingQuestion v-else :question-num="currentQuestion - 1" @increment="nextQuestion"
-      @decrement="prevQuestion" />
-    <ProgressNodes :active="currentQuestion - 1" />
+
+    <!-- Onboarding questions -->
+    <OnboardingQuestion
+      v-else
+      :question-num="currentQuestion - 1"
+      @increment="nextQuestion"
+      @decrement="prevQuestion"
+    />
+    <ProgressNodes :active="currentQuestion - 1" class="mt-24" />
   </div>
 </template>
 
 <script setup lang="ts">
-  import {ref} from "vue";
-  import ProgressNodes from "./onboarding/ProgressNodes.vue";
-  import OnboardingQuestion from "./onboarding/OnboardingQuestion.vue";
+import { ref } from "vue";
+import ProgressNodes from "./onboarding/ProgressNodes.vue";
+import OnboardingQuestion from "./onboarding/OnboardingQuestion.vue";
 
-  const currentQuestion = ref(0);
-  const props = defineProps < {
-    visible: boolean;
-  } > ();
+const currentQuestion = ref(0);
+const props = defineProps<{
+  visible: boolean;
+}>();
 
-  const dateOfBirth = ref();
-  const height = ref(0.0);
-  const weight = ref(0.0);
-  const gender = ref("");
+const dateOfBirth = ref();
+const height = ref(0.0);
+const weight = ref(0.0);
+const gender = ref("");
 
-  const nextQuestion = () => {
-    currentQuestion.value =
-      currentQuestion.value < 5
-        ? currentQuestion.value + 1
-        : currentQuestion.value;
-  };
+const nextQuestion = () => {
+  currentQuestion.value =
+    currentQuestion.value < 5
+      ? currentQuestion.value + 1
+      : currentQuestion.value;
+};
 
-  const prevQuestion = () => {
-    currentQuestion.value =
-      currentQuestion.value > 0
-        ? currentQuestion.value - 1
-        : currentQuestion.value;
-  };
+const prevQuestion = () => {
+  currentQuestion.value =
+    currentQuestion.value > 0
+      ? currentQuestion.value - 1
+      : currentQuestion.value;
+};
 </script>
--- src/components/onboarding/OnboardingQuestion.vue
+++ src/components/onboarding/OnboardingQuestion.vue
@@ -8,42 +8,52 @@
     <p><span>Subtitle: </span> {{ questions[props.questionNum].subtitle }}</p>
   </div>
   <div>
-    <button @click="$emit('decrement')">Back</button>
-    <button @click="$emit('increment')">Continue</button>
+    <button @click="goBackward">Back</button>
+    <button @click="goForward">Continue</button>
   </div>
 </template>
 
 <script setup lang="ts">
-  const props = defineProps < {
-    questionNum: number;
-  } > ();
+const props = defineProps<{
+  questionNum: number;
+}>();
 
-  const questions = [
-    {
-      title: "What is your date of birth?",
-      image: "cake",
-      input: "date",
-      subtitle:
-        "This will help Bite Balance Calculate your age for BMI and nutritional needs.",
-    },
-    {
-      title: "What is your current height?",
-      image: "plants",
-      input: "height",
-      subtitle: "",
-    },
-    {
-      title: "What is your current weight?",
-      image: "scale",
-      input: "weight",
-      subtitle: "",
-    },
-    {
-      title: "Would you like to share your gender?",
-      image: "question",
-      input: "gender",
-      subtitle:
-        "This can help Bite Balance fine-tune BMI calculations based on gender-related standards.",
-    },
-  ];
+const emit = defineEmits(["increment", "decrement"]);
+
+const goForward = () => {
+  emit("increment");
+};
+
+const goBackward = () => {
+  emit("decrement");
+};
+
+const questions = [
+  {
+    title: "What is your date of birth?",
+    image: "cake",
+    input: "date",
+    subtitle:
+      "This will help Bite Balance Calculate your age for BMI and nutritional needs.",
+  },
+  {
+    title: "What is your current height?",
+    image: "plants",
+    input: "height",
+    subtitle: "",
+  },
+  {
+    title: "What is your current weight?",
+    image: "scale",
+    input: "weight",
+    subtitle: "",
+  },
+  {
+    title: "Would you like to share your gender?",
+    image: "question",
+    input: "gender",
+    subtitle:
+      "This can help Bite Balance fine-tune BMI calculations based on gender-related standards.",
+  },
+];
 </script>
