7bcdeea80c814d2cfb1a5b774da0dd8f6f458d04
miguel-fg <miguelfierro55@gmail.com>
2024-11-29 23:02:37 -0800

    Add progress nodes svg

--- src/components/onboarding/ProgressNodes.vue
+++ src/components/onboarding/ProgressNodes.vue
@@ -1,11 +1,62 @@
 <template>
-  <div>Node: {{ active }}</div>
+  <div class="flex items-center">
+    <svg
+      :width="(nodes.length - 1) * spacing + 2 * radius"
+      :height="radius * 2"
+      class="relative"
+    >
+      <line
+        v-for="i in nodes.length - 1"
+        :key="'line-' + i"
+        :x1="(i - 1) * spacing + radius"
+        :y1="radius"
+        :x2="i * spacing + radius"
+        :y2="radius"
+        :stroke="i - 1 < props.active ? '#30CfC0' : '#D0D9D6'"
+        stroke-width="4"
+      >
+        <animate
+          v-if="i - 1 < active"
+          attribute-name="stroke-dasharray"
+          from="0, 100"
+          to="100, 0"
+          dur="0.5s"
+          fill="freeze"
+        />
+      </line>
+
+      <circle
+        v-for="(_, index) in nodes"
+        :key="'circle-' + index"
+        :cx="index * spacing + radius"
+        :cy="radius"
+        :r="radius"
+        :stroke="index === active ? '#30CFC0' : ''"
+        :stroke-width="index === active ? '4' : '0'"
+        :fill="
+          index < active ? '#30CFC0' : index === active ? 'white' : '#D0D9D6'
+        "
+      >
+        <animate
+          v-if="index < active"
+          attributeName="fill"
+          from="#D0D9D6"
+          to="#30CFC0"
+          dur="0.5s"
+          fill="freeze"
+        />
+      </circle>
+    </svg>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import {ref} from "vue";
+const props = defineProps<{
+  active: number;
+}>();
 
-  const props = defineProps < {
-    active: number;
-  } > ();
+const nodes = Array(4).fill(null);
+
+const spacing = 80;
+const radius = 15;
 </script>
