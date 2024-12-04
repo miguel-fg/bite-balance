8efb63ec6a82a445d84ea4083c062c2cdf57d825
miguel-fg <miguelfierro55@gmail.com>
2024-12-03 17:45:49 -0800

    Add responsive layout

--- src/components/Onboarding.vue
+++ src/components/Onboarding.vue
@@ -1,73 +1,119 @@
 <template>
-  <div v-if="props.visible" class="z-50 inset-0 fixed flex flex-col justify-center px-4 bg-ui-gray-100">
-    <!-- Welcome to Onboarding quiz -->
-    <div v-if="currentQuestion === 0" class="flex flex-col gap-12">
-      <h1 class="font-MT font-bold text-title text-ui-gray-900">
-        Welcome to Bite Balance
-      </h1>
-      <img src="/bb_logo_dark.png" alt="Bite Balance logo" class="w-24 h-24" />
-      <div class="flex flex-col gap-1">
-        <h1 class="font-MK font-normal text-subtitle text-ui-gray-900">
-          Let's get to know you better!
+  <div
+    v-if="props.visible"
+    class="z-50 inset-0 fixed flex items-center justify-center px-4 bg-ui-gray-100"
+  >
+    <div
+      class="rounded-md md:px-12 md:py-6 md:shadow-md md:w-6/12 md:bg-[#F9F9F9] md:max-w-md"
+    >
+      <!-- Welcome to Onboarding quiz -->
+      <div v-if="currentQuestion === 0" class="flex flex-col gap-12">
+        <h1 class="font-MT font-bold text-title text-ui-gray-900">
+          Welcome to Bite Balance
         </h1>
-        <p class="font-HM font-light text-body text-ui-gray-700">
-          Answer these quick questions to personalize your nutrition tracker
-          experience.
-        </p>
-      </div>
-      <div class="flex justify-end gap-4">
-        <button @click="$emit('closeOnboarding')" class="btn text-ui-gray-700">
-          Skip
-        </button>
-        <button @click="nextQuestion" class="btn bg-primary-500 text-ui-gray-900 shadow">
-          Get Started
-        </button>
-      </div>
-    </div>
-
-    <!-- Summary of Onboarding user answers -->
-    <div v-else-if="currentQuestion === 5" class="flex flex-col gap-12">
-      <h1 class="font-MK font-normal text-subtitle text-ui-gray-900">
-        Summary
-      </h1>
-      <div>
-        <div
-          class="flex flex-col divide-y px-4 rounded-md font-HM font-light text-body bg-white shadow text-ui-gray-900">
-          <div class="flex justify-between py-4">
-            <span class="font-bold" :class="invalidDOB ? 'text-danger-500' : 'text-primary-700'">Date of Birth:</span>
-            {{ dateOfBirth }}
-          </div>
-          <div class="flex justify-between py-4">
-            <span class="font-bold" :class="invalidHeight ? 'text-danger-500' : 'text-primary-700'">Height:</span>
-            {{ `${height.value} ${height.unit}` }}
-          </div>
-          <div class="flex justify-between py-4">
-            <span class="font-bold" :class="invalidWeight ? 'text-danger-500' : 'text-primary-700'">Weight:</span>
-            {{ `${weight.value} ${weight.unit}` }}
-          </div>
-          <div class="flex justify-between py-4">
-            <span class="font-bold text-primary-700">Gender:</span> {{ gender }}
-          </div>
+        <img
+          src="/bb_logo_dark.png"
+          alt="Bite Balance logo"
+          class="w-24 h-24"
+        />
+        <div class="flex flex-col gap-1">
+          <h1 class="font-MK font-normal text-subtitle text-ui-gray-900">
+            Let's get to know you better!
+          </h1>
+          <p class="font-HM font-light text-body text-ui-gray-700">
+            Answer these quick questions to personalize your nutrition tracker
+            experience.
+          </p>
         </div>
-        <p v-if="!answersValid" class="mt-1 font-HM font-light text-body text-danger-500">
-          Some of your answers are not valid.
-        </p>
+        <div class="flex justify-end gap-4">
+          <button
+            @click="$emit('closeOnboarding')"
+            class="btn text-ui-gray-700"
+          >
+            Skip
+          </button>
+          <button
+            @click="nextQuestion"
+            class="btn bg-primary-500 text-ui-gray-900 shadow"
+          >
+            Get Started
+          </button>
+        </div>
       </div>
-      <div class="flex justify-end gap-4">
-        <button @click="prevQuestion" class="btn text-ui-gray-700">Back</button>
-        <button @click="$emit('closeOnboarding')" :disabled="!answersValid" class="btn shadow" :class="!answersValid
-            ? 'bg-primary-200 text-ui-gray-500'
-            : 'bg-primary-500 text-ui-gray-900'
-          ">
-          Finish
-        </button>
-      </div>
-    </div>
 
-    <!-- Onboarding questions -->
-    <OnboardingQuestion v-else :question-num="currentQuestion - 1" @increment="nextQuestion"
-      @decrement="prevQuestion" />
-    <ProgressNodes :active="currentQuestion - 1" class="mt-24" />
+      <!-- Summary of Onboarding user answers -->
+      <div v-else-if="currentQuestion === 5" class="flex flex-col gap-12">
+        <h1 class="font-MK font-normal text-subtitle text-ui-gray-900">
+          Summary
+        </h1>
+        <div>
+          <div
+            class="flex flex-col divide-y px-4 rounded-md font-HM font-light text-body bg-white shadow text-ui-gray-900"
+          >
+            <div class="flex justify-between py-4">
+              <span
+                class="font-bold"
+                :class="invalidDOB ? 'text-danger-500' : 'text-primary-700'"
+                >Date of Birth:</span
+              >
+              {{ dateOfBirth }}
+            </div>
+            <div class="flex justify-between py-4">
+              <span
+                class="font-bold"
+                :class="invalidHeight ? 'text-danger-500' : 'text-primary-700'"
+                >Height:</span
+              >
+              {{ `${height.value} ${height.unit}` }}
+            </div>
+            <div class="flex justify-between py-4">
+              <span
+                class="font-bold"
+                :class="invalidWeight ? 'text-danger-500' : 'text-primary-700'"
+                >Weight:</span
+              >
+              {{ `${weight.value} ${weight.unit}` }}
+            </div>
+            <div class="flex justify-between py-4">
+              <span class="font-bold text-primary-700">Gender:</span>
+              {{ gender }}
+            </div>
+          </div>
+          <p
+            v-if="!answersValid"
+            class="mt-1 font-HM font-light text-body text-danger-500"
+          >
+            Some of your answers are not valid.
+          </p>
+        </div>
+        <div class="flex justify-end gap-4">
+          <button @click="prevQuestion" class="btn text-ui-gray-700">
+            Back
+          </button>
+          <button
+            @click="$emit('closeOnboarding')"
+            :disabled="!answersValid"
+            class="btn shadow"
+            :class="
+              !answersValid
+                ? 'bg-primary-200 text-ui-gray-500'
+                : 'bg-primary-500 text-ui-gray-900'
+            "
+          >
+            Finish
+          </button>
+        </div>
+      </div>
+
+      <!-- Onboarding questions -->
+      <OnboardingQuestion
+        v-else
+        :question-num="currentQuestion - 1"
+        @increment="nextQuestion"
+        @decrement="prevQuestion"
+      />
+      <ProgressNodes :active="currentQuestion - 1" class="mt-24" />
+    </div>
   </div>
 </template>
 
@@ -138,6 +184,6 @@ const prevQuestion = () => {
 };
 
 const answersValid = computed(
-  () => !invalidDOB.value || !invalidHeight.value || !invalidWeight.value,
+  () => !invalidDOB.value && !invalidHeight.value && !invalidWeight.value,
 );
 </script>
