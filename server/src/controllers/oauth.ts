ddf9bc7f78ff84617c8df3d84a3992d1d103f940
miguel-fg <miguelfierro55@gmail.com>
2024-12-11 16:11:06 -0800

    Integrate GitHub OAuth

--- server/src/controllers/oauth.ts
+++ server/src/controllers/oauth.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import prisma from "../services/prisma";
 import { GitHub, Google } from "arctic";
+import { generateState, generateCodeVerifier } from "arctic";
+import { OAuth2RequestError, ArcticFetchError } from "arctic";
 import {
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
 } from "src/config";
+import {
+  generateSessionToken,
+  createSession,
+  setSessionTokenCookie,
+} from "src/services/session";
 
 const github = new GitHub(
   GITHUB_CLIENT_ID,
@@ -19,3 +26,129 @@ const google = new Google(
   GOOGLE_CLIENT_SECRET,
   "http://localhost:3000/api/oauth/google/callback",
 );
+
+export function redirectGitHub(req: Request, res: Response) {
+  const isProd = process.env.NODE_ENV === "production";
+
+  const state = generateState();
+
+  const scopes = ["user:email"];
+  const url = github.createAuthorizationURL(state, scopes);
+
+  const cookieOptions = [
+    `state=${state}`,
+    `HttpOnly`,
+    `SameSite=Lax`,
+    `Max-Age=${60 * 10}`,
+    `Path=/`,
+    isProd ? `Secure` : "",
+  ]
+    .filter(Boolean)
+    .join("; ");
+
+  res.setHeader("Set-Cookie", cookieOptions);
+
+  res.redirect(url.href);
+}
+
+export async function validateGitHubAuthCode(req: Request, res: Response) {
+  interface GitHubUserProfile {
+    login: string;
+    id: number;
+    avatar_url: string;
+    email?: string;
+  }
+
+  interface GitHubEmail {
+    email: string;
+    verified: boolean;
+    primary: boolean;
+    visibility: string | null;
+  }
+
+  const { code, state } = req.query;
+
+  const savedState = req.cookies.state;
+
+  if (!savedState || savedState !== state) {
+    res.status(400).json({ message: "Invalid or missing state" });
+    return;
+  }
+
+  try {
+    const tokens = await github.validateAuthorizationCode(String(code));
+    const accessToken = tokens.accessToken();
+
+    const userResponse = await fetch("https://api.github.com/user", {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    const userProfile = (await userResponse.json()) as GitHubUserProfile;
+
+    if (!userProfile.email) {
+      const emailResponse = await fetch("https://api.github.com/user/emails", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+
+      const emails = (await emailResponse.json()) as GitHubEmail[];
+      userProfile.email = emails.find((email) => email.primary)?.email;
+    }
+
+    let user = await prisma.user.findUnique({
+      where: { email: userProfile.email },
+    });
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          email: userProfile.email,
+          username: userProfile.login,
+          provider: "github",
+          providerId: String(userProfile.id),
+        },
+      });
+    }
+
+    const sessionToken = generateSessionToken();
+    const session = await createSession(sessionToken, user.id);
+
+    setSessionTokenCookie(res, sessionToken, session.expiresAt);
+
+    res.redirect("http://localhost:5173");
+  } catch (error) {
+    if (error instanceof OAuth2RequestError) {
+      res.status(400).json({ message: "Invalid authorization code" });
+    } else if (error instanceof ArcticFetchError) {
+      res.status(500).json({ message: "Error fetching data from GitHub" });
+    } else {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
+
+export function redirectGoogle(req: Request, res: Response) {
+  const isProd = process.env.NODE_ENV === "production";
+
+  const state = generateState();
+  const codeVerifier = generateCodeVerifier();
+
+  const scopes = ["profile"];
+  const url = google.createAuthorizationURL(state, codeVerifier, scopes);
+
+  const cookieOptions = [
+    `state=${state}`,
+    `HttpOnly`,
+    `SameSite=Lax`,
+    `Max-Age=${60 * 10}`,
+    `Path=/`,
+    isProd ? `Secure` : "",
+  ]
+    .filter(Boolean)
+    .join("; ");
+
+  res.setHeader("Set-Cookie", cookieOptions);
+
+  res.redirect(url.href);
+}
+
+export function validateGoogleAuthCode(req: Request, res: Response) {}
--- server/src/routes/oauth.ts
+++ server/src/routes/oauth.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import {
+  redirectGoogle,
+  validateGoogleAuthCode,
+  redirectGitHub,
+  validateGitHubAuthCode,
+} from "src/controllers/oauth";
 
 const router = Router();
 
-router.get("/google", () => {});
-router.get("/google/callback", () => {});
+router.get("/google", redirectGoogle);
+router.get("/google/callback", validateGoogleAuthCode);
 
-router.get("/github", () => {});
-router.get("/github/callback", () => {});
+router.get("/github", redirectGitHub);
+router.get("/github/callback", validateGitHubAuthCode);
 
 export default router;
